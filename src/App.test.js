d64c6bf96f5f13303fb2c457f05aaf17f9d5b1cb
mountither <monti_123@live.com>
2021-06-19 15:15:39 +1000

    feat: add semantic release + temp test

--- src/App.test.js
+++ src/App.test.js
@@ -3,8 +3,6 @@ import { screen } from '@testing-library/react';
 import { render } from './test-utils';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn chakra/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('add', () => {
+  expect((1 + 2)).toEqual(3)
+})
