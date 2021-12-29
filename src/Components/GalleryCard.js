b55c487a6cb61fc74cc63e40c55ac7a07bfd404f
mountither <monti_123@live.com>
2021-12-29 19:09:36 +1100

    feat(gallery): add musuem reference logo + url for each artefact

--- src/Components/GalleryCard.js
+++ src/Components/GalleryCard.js
@@ -10,77 +10,133 @@ import {
   Tr,
   Td,
   useColorMode,
+  Flex,
+  Text,
 } from '@chakra-ui/react';
 
 // params: the content to be shown on card.
 // reqs: image, date
 // others are props
 
+const museumsDetails = {
+  MET: {
+    logo: '/images/museum-logos/met-logo.svg',
+    url: 'https://www.metmuseum.org/',
+  },
+  VAM: {
+    logo: '/images/museum-logos/vam-logo.svg',
+    url: 'https://www.vam.ac.uk/',
+  },
+  HAM: {
+    logo: '/images/museum-logos/ham-logo.jpeg',
+    url: 'https://harvardartmuseums.org/',
+  },
+  SMG: {
+    logo: '/images/museum-logos/smg-logo.png',
+    url: 'https://www.sciencemuseumgroup.org.uk/',
+  },
+  MVC: {
+    logo: '/images/museum-logos/mvc-logo.svg',
+    url: 'https://museumsvictoria.com.au/',
+  },
+  AIC: {
+    logo: '/images/museum-logos/aic-logo.png',
+    url: 'https://www.artic.edu/',
+  },
+};
 
-const GalleryCard = ({title, imgURL, dateCreation, region,  restPayload}) =>{
+const GalleryCard = ({
+  title,
+  imgURL,
+  dateCreation,
+  region,
+  restPayload,
+  museum,
+}) => {
+  const { colorMode } = useColorMode();
 
-
-    const { colorMode } = useColorMode()
-
-    return (
-
-    <Box boxShadow={colorMode === 'dark' ? 'dark-lg' :'sm'} borderRadius="md" maxW="70rem" overflow="hidden" padding={5} >
-        <Heading 
-          mb={4} 
-          size='sm'
-          fontSize={{ base:'15px', lg: "25px" }}
-        >
+  return (
+    <Box
+      boxShadow={colorMode === 'dark' ? 'dark-lg' : 'sm'}
+      borderRadius="md"
+      maxW="70rem"
+      padding={5}
+    >
+      <Flex
+        justifyContent={'space-between'}
+        direction={'row'}
+        alignItems={'center'}
+        mb={2}
+        gridGap={4}
+      >
+        <Heading size="sm" fontSize={{ base: '15px', lg: '25px' }}>
           {title ? title : 'Untitled'}
         </Heading>
+        <Flex direction={'row'} alignItems={'center'} gridGap={3}>
+          <Text size="sm" color={'gray.400'}>
+            From:{' '}
+          </Text>
+          <Box
+            as="a"
+            target="_blank"
+            href={museumsDetails[museum].url}
+            cursor={'pointer'}
+            width="50px"
+          >
+            <Image
+              src={museumsDetails[museum].logo}
+              alt={`${museum} Logo`}
+              width="100%"
+              objectFit="contain"
+              padding={'5px'}
+              borderRadius={7}
+              _hover={{ opacity: 0.6 }}
+              transition="opacity .6s"
+            />
+          </Box>
+        </Flex>
+      </Flex>
 
-        <Stack  maxHeight="100%" overflow="hidden" maxWidth='100vw'>
-        
-        <Image _hover={{transform: 'scale(1.03)'}}  
-        transition='transform .6s' 
-        objectFit="contain"  
-        src={imgURL}/>
-        </Stack>
-        
+      <Stack maxHeight="100%" overflow="hidden" maxWidth="100vw">
+        <Image
+          _hover={{ transform: 'scale(1.03)' }}
+          transition="transform .6s"
+          objectFit="contain"
+          src={imgURL}
+        />
+      </Stack>
 
-        <Center >
-            <Stack spacing={4}  direction={{ base:"column"}} align='center' >
-
-
-            <Table>
-              <Tbody fontSize={{ base:'10px', lg: "15px" }}>
-               {dateCreation ? 
+      <Center>
+        <Stack spacing={4} direction={{ base: 'column' }} align="center">
+          <Table>
+            <Tbody fontSize={{ base: '10px', lg: '15px' }}>
+              {dateCreation ? (
                 <Tr>
-                    <Td fontWeight='semibold'>Date Created</Td>
-                    <Td>{dateCreation}</Td>
-                  </Tr> : null
-                }
-                {
-                region ?
+                  <Td fontWeight="semibold">Date Created</Td>
+                  <Td>{dateCreation}</Td>
+                </Tr>
+              ) : null}
+              {region ? (
                 <Tr>
-                  <Td fontWeight='semibold'>Region</Td>
+                  <Td fontWeight="semibold">Region</Td>
                   <Td>{region}</Td>
-                </Tr> : null
-                }
-                {
-                  restPayload && 
-                    restPayload.map((o, i) => (
-                    o?.data ? 
+                </Tr>
+              ) : null}
+              {restPayload &&
+                restPayload.map((o, i) =>
+                  o?.data ? (
                     <Tr key={i}>
-                      <Td fontWeight='semibold'>{o.type}</Td>
+                      <Td fontWeight="semibold">{o.type}</Td>
                       <Td>{o.data}</Td>
-                    </Tr> : null
-                    ))
-              }
-              </Tbody>
-          
-            </Table>
-
-        
-            </Stack>
-        </Center>
-
-  </Box>
-    ) 
-}
+                    </Tr>
+                  ) : null
+                )}
+            </Tbody>
+          </Table>
+        </Stack>
+      </Center>
+    </Box>
+  );
+};
 
 export default GalleryCard;
--- src/Pages/Gallery.js
+++ src/Pages/Gallery.js
@@ -1,187 +1,183 @@
 import React from 'react';
-import {
-  VStack,
-  Grid,
-  Divider,
-  Button
-} from '@chakra-ui/react';
+import { VStack, Grid, Divider, Button } from '@chakra-ui/react';
 
+import SkeletonCards from '../Components/Skeletons/SkeletonCards';
 
-import SkeletonCards from "../Components/Skeletons/SkeletonCards";
+import useMuseumData from '../API/Museums';
 
-import useMuseumData from '../API/Museums'  
-
-import {
-  useLocation,
-  useHistory,
-  Redirect
-} from "react-router-dom";
+import { useLocation, useHistory, Redirect } from 'react-router-dom';
 
 import AlertContent from '../Components/AlertContent';
 import GalleryCard from '../Components/GalleryCard';
 
-import Heading from '../Components/Typography/Heading'
+import Heading from '../Components/Typography/Heading';
 
 // TODO - Implement a Load more feature (pagination)
 
-const Gallery = ()=> {
-
+const Gallery = () => {
   const location = useLocation();
   const history = useHistory();
 
-  const {isLoading, data, error} = useMuseumData();
+  const { isLoading, data, error } = useMuseumData();
 
   return (
     <>
-    {(location.state == undefined || location.state == null || location.state == '') ? 
-    
-    <Redirect to='/'/> 
-    
-    :
+      {location.state == undefined ||
+      location.state == null ||
+      location.state == '' ? (
+        <Redirect to="/" />
+      ) : (
+        <VStack spacing={10} padding={3}>
+          <Button
+            fontWeight="semibold"
+            fontSize="15px"
+            variant="link"
+            onClick={() => history.goBack()}
+          >
+            Back
+          </Button>
 
-    <VStack spacing={10} padding={3}>
+          <Grid
+            templateRows="1fr 0.2fr"
+            templateColumns="1fr 1fr"
+            gridAutoFlow="dense"
+            gap={0}
+          >
+            <Heading
+              main
+              marginTop={10}
+              gridArea={'1/1/2/3'}
+              marginLeft={5}
+              title={location.state.title || 'Untitled'}
+            />
 
-           <Button fontWeight='semibold' fontSize='15px'
-           variant='link'
-           onClick={()=> history.goBack()}
-            >
-             Back
-             </Button>
+            <Divider />
 
-              <Grid
-                templateRows="1fr 0.2fr"
-                templateColumns="1fr 1fr"
-                gridAutoFlow='dense'
-                gap={0}>
-            
-              <Heading      
-                main
-                marginTop={10} 
-                gridArea={'1/1/2/3'} 
-                marginLeft={5}  
-                title={location.state.title || 'Untitled'} 
-              />
-               
-               <Divider/>
+            {error && error.length !== 0 && (
+              <AlertContent errors={error} zIndex={10} gridArea={'2/2/3/3'} />
+            )}
 
-                {error && error.length !== 0
-                && <AlertContent errors={error}  zIndex={10} gridArea={'2/2/3/3'}/>}   
-
-                {/* <Text paddingLeft={5} fontSize={{ base:'15px', lg: "25px", md:"25px" }}>
+            {/* <Text paddingLeft={5} fontSize={{ base:'15px', lg: "25px", md:"25px" }}>
                   {`Results: ${metArtData.length+vamArtData.length+hamArtData.length+aicArtData.length}`}
                 </Text> */}
-
           </Grid>
 
-          {
-            isLoading ? <SkeletonCards /> : 
-            data['MET']?.map((a, i) =>(
-                
-                <GalleryCard 
-                  key={i} 
-                  title={a.title} 
-                  imgURL={a.primaryImageSmall}
-                  dateCreation={a.objectDate}
-                  region={a.region} 
-                  restPayload={[
-                    a.culture && {type: 'Culture', data:a.culture},
-                    a.medium && {type: 'Type', data:a.medium},
-                    a.period && {type: 'Period', data:a.period}
-                  ]}
-                />
-
+          {isLoading ? (
+            <SkeletonCards />
+          ) : (
+            data['MET']?.map((a, i) => (
+              <GalleryCard
+                key={i}
+                museum={'MET'}
+                title={a.title}
+                imgURL={a.primaryImageSmall}
+                dateCreation={a.objectDate}
+                region={a.region}
+                restPayload={[
+                  a.culture && { type: 'Culture', data: a.culture },
+                  a.medium && { type: 'Type', data: a.medium },
+                  a.period && { type: 'Period', data: a.period },
+                ]}
+              />
             ))
-            }
+          )}
 
-            {
-              isLoading ? <SkeletonCards /> : 
-              data['VAM']?.map((a, i) => (
-                  <GalleryCard 
-                    key={i} 
-                    title={a._primaryTitle ? a._primaryDate :a.objectType} 
-                    imgURL={`https://framemark.vam.ac.uk/collections/${a._primaryImageId}/full/735,/0/default.jpg`}
-                    dateCreation={a._primaryDate}
-                    region={a._primaryPlace} 
-                    restPayload={[
-                      a.objectType && {type: 'Type', data:a.objectType}
-                    ]}
-                  />
+          {isLoading ? (
+            <SkeletonCards />
+          ) : (
+            data['VAM']?.map((a, i) => (
+              <GalleryCard
+                key={i}
+                museum={'VAM'}
+                title={a._primaryTitle ? a._primaryDate : a.objectType}
+                imgURL={`https://framemark.vam.ac.uk/collections/${a._primaryImageId}/full/735,/0/default.jpg`}
+                dateCreation={a._primaryDate}
+                region={a._primaryPlace}
+                restPayload={[
+                  a.objectType && { type: 'Type', data: a.objectType },
+                ]}
+              />
+            ))
+          )}
 
+          {isLoading ? (
+            <SkeletonCards />
+          ) : (
+            data['HAM']?.map((a, i) => (
+              <GalleryCard
+                key={i}
+                museum={'HAM'}
+                title={a.title}
+                imgURL={a.primaryimageurl}
+                dateCreation={a.dated}
+                restPayload={[
+                  a.classification && {
+                    type: 'Classification',
+                    data: a.classification,
+                  },
+                  a.culture && { type: 'Culture', data: a.culture },
+                ]}
+              />
+            ))
+          )}
 
-                ))
-            }
-
-          {
-          
-          isLoading ? <SkeletonCards /> : 
-          data['HAM']?.map((a, i) => (
-                <GalleryCard 
-                  key={i} 
-                  title={a.title} 
-                  imgURL={a.primaryimageurl}
-                  dateCreation={a.dated}
-                  restPayload={[
-                    a.classification && {type: 'Classification', data:a.classification},
-                    a.culture && {type: 'Culture', data: a.culture}           
-                  ]}
-                />
-
-              ))
-          }
-
-          
-          {
-            !isLoading &&
+          {!isLoading &&
             data['SMG']?.map((a, i) => (
-                <GalleryCard 
-                  key={i} 
-                  title={a.attributes.summary_title} 
-                  imgURL={`https://coimages.sciencemuseumgroup.org.uk/images/${a.attributes.multimedia[0]?.processed.large.location}`}
-                  dateCreation={(a.attributes.lifecycle?.creation?.date) && a.attributes.lifecycle.creation[0].date[0].value}
-                  restPayload={[
-                    a.attributes.categories && {type: 'Category', data: a.attributes.categories[0].name}
-                  ]}
-                />
+              <GalleryCard
+                key={i}
+                museum={'SMG'}
+                title={a.attributes.summary_title}
+                imgURL={`https://coimages.sciencemuseumgroup.org.uk/images/${a.attributes.multimedia[0]?.processed.large.location}`}
+                dateCreation={
+                  a.attributes.lifecycle?.creation?.date &&
+                  a.attributes.lifecycle.creation[0].date[0].value
+                }
+                restPayload={[
+                  a.attributes.categories && {
+                    type: 'Category',
+                    data: a.attributes.categories[0].name,
+                  },
+                ]}
+              />
+            ))}
 
-              ))
-          }
-
-          {
-            !isLoading &&
+          {!isLoading &&
             data['MVC']?.map((a, i) => (
-                <GalleryCard 
-                  key={i} 
-                  title={a?.displayTitle} 
-                  imgURL={a?.media[0]?.large?.uri}
-                  dateCreation={a?.associations[0]?.date}
-                  region={a?.associations[0]?.country}
-                />
+              <GalleryCard
+                key={i}
+                museum={'MVC'}
+                title={a?.displayTitle}
+                imgURL={a?.media[0]?.large?.uri}
+                dateCreation={a?.associations[0]?.date}
+                region={a?.associations[0]?.country}
+              />
+            ))}
 
-              ))
-          }
-
-          {
-            !isLoading &&
+          {!isLoading &&
             data['AIC']?.map((a, i) => (
-                <GalleryCard 
-                  key={i} 
-                  title={a.title} 
-                  imgURL={a.image_id && `https://www.artic.edu/iiif/2/${a.image_id}/full/843,/0/default.jpg`}
-                  dateCreation={a.date_display}
-                  region={a.place_of_origin}
-                  restPayload={[
-                    a.medium_display &&  {type: 'Type', data: a.medium_display},
-                    a.artist_display && {type: 'Creator', data: a.artist_display},
-                  ]}
-                />
-
-              ))
-          }
-       
-          </VStack>
-        }
-        </>
+              <GalleryCard
+                key={i}
+                museum={'AIC'}
+                title={a.title}
+                imgURL={
+                  a.image_id &&
+                  `https://www.artic.edu/iiif/2/${a.image_id}/full/843,/0/default.jpg`
+                }
+                dateCreation={a.date_display}
+                region={a.place_of_origin}
+                restPayload={[
+                  a.medium_display && { type: 'Type', data: a.medium_display },
+                  a.artist_display && {
+                    type: 'Creator',
+                    data: a.artist_display,
+                  },
+                ]}
+              />
+            ))}
+        </VStack>
+      )}
+    </>
   );
-}
+};
 
 export default Gallery;
